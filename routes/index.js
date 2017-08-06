941a22af2021c3cd46eaf5430c878b360d14fdec
Kyle <kyle.c.r.fahey@gmail.com>
2017-08-05 22:52:19 -0500

    Add REDIS

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,62 @@
 var express = require('express');
 var router = express.Router();
 
+var redis = require("redis"),
+    client = redis.createClient();
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log(req.session)
   res.render('index', { title: 'Express' });
 });
 router.get('/lol', function(req, res, next) {
-  console.log(req.sessionID)
-  res.render('index', { title: req.session });
+  res.render('index', { title: req.sessionID });
 });
+
+
+/* 
+
+    Redis History 
+
+
+*/
+
+
+router.post('/history', function(req, res) {
+  let history = req.body.history
+  let sesh = req.sessionID
+
+  console.log(history)
+  console.log(sesh)
+
+  client.hset(sesh, "history", history ,redis.print);
+
+  client.on('error', function(err){
+    console.log(err)
+    res.status().send({"status":"error"})
+  })
+
+  res.status(200).send({"status":"success"})
+})
+
+router.post('/client', function(req, res) {
+  let history = req.body.data
+  let sesh = req.sessionID
+
+  console.log(history)
+  console.log(sesh)
+
+  client.hset(sesh, "client", history , redis.print);
+
+  client.on('error', function(err){
+    console.log(err)
+    res.status().send({"status":"error"})
+  })
+
+  res.status(200).send({"status":"success"})
+})
+
+
+
+
 module.exports = router;
