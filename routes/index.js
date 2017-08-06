faac2202f1e55830f92c8e1ee46f862e996ca8a2
Kyle <kyle.c.r.fahey@gmail.com>
2017-08-05 23:00:39 -0500

    add important things like personality insights watson api

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,14 @@ var router = express.Router();
 var redis = require("redis"),
     client = redis.createClient();
 
+var PersonalityInsightsV3 = require('watson-developer-cloud/personality-insights/v3');
+
+var personality_insights = new PersonalityInsightsV3({
+  username: '<username>',
+  password: '<password>',
+  version_date: '2016-10-19'
+});
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -18,10 +26,7 @@ router.get('/lol', function(req, res, next) {
 
     Redis History 
 
-
 */
-
-
 router.post('/history', function(req, res) {
   let history = req.body.history
   let sesh = req.sessionID
@@ -39,21 +44,38 @@ router.post('/history', function(req, res) {
   res.status(200).send({"status":"success"})
 })
 
+
+/* 
+
+  Core Call 
+
+*/
+
 router.post('/client', function(req, res) {
   let history = req.body.data
   let sesh = req.sessionID
+  let profile
 
   console.log(history)
   console.log(sesh)
 
   client.hset(sesh, "client", history , redis.print);
 
-  client.on('error', function(err){
-    console.log(err)
-    res.status().send({"status":"error"})
-  })
 
-  res.status(200).send({"status":"success"})
+  personality_insights.profile({
+  text: 'Enter more than 100 unique words here...',
+  consumption_preferences: true
+  },
+  function (err, response) {
+    if (err)
+      console.log('error:', err);
+    else
+      console.log(JSON.stringify(response, null, 2));
+      res.status(200).send({"status":"success", "data": response})
+});
+
+
+ 
 })
 
 
